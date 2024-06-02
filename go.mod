02ee5d4877d166d1a2bb93ec1d83a015eb162267
Vitor Aguiar <vitoraguiar.df@gmail.com>
2024-06-02 11:12:29 -0300

    Golang startup

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/vitoraguiardf/golang-quick-start-gorm
+
+go 1.22.3
+
+require github.com/go-playground/assert/v2 v2.2.0
---
+++ main_test.go
@@ -0,0 +1,11 @@
+package main_test
+
+import (
+	"testing"
+
+	"github.com/go-playground/assert/v2"
+)
+
+func TestMain(t *testing.T) {
+	assert.Equal(t, true, true)
+}
